2f9e9931051992c82630f6917176d04acd2710fd
NivekNK <kevin.rojas.v@mail.pucv.cl>
2023-04-23 16:15:05 -0400

    Update

--- js/botones.js
+++ js/botones.js
@@ -1,3 +1,7 @@
+$("button[name='registrar']").click(function() {
+    window.location.href = "registro.html";
+});
+
 $("button[name='ingresar']").click(function() {
     window.location.href = "login.html";
 });
