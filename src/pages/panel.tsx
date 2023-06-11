b82a3e94dfdee8a57da179563dd2f0eb96652618
Jan <jan.houter.h@mail.pucv.cl>
2023-06-11 18:43:30 -0400

    Panel con React

---
+++ src/pages/panel.tsx
@@ -0,0 +1,121 @@
+function Panel() {
+    return (
+        <div>
+            <div className="content">
+                <div className="inner-content">
+                    <div className="row">
+                        <div className="col-xl-3 col-md-6">
+                            <div className="card">
+                                <div className="card-block">
+                                    <div className="row align-items-center">
+                                        <div className="col-8">
+                                            <div className="card-inner-text">
+                                                <h4 className="text-c-yellow f-w-600">$30200</h4>
+                                                <h6 className="text-muted m-b-0">All Earnings</h6>
+                                            </div>
+                                        </div>
+                                        <div className="col-4 text-right">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-footer bg-c-yellow">
+                                    <div className="row align-items-center">
+                                        <div className="col-9">
+                                            <p className="text-white m-b-0">% change</p>
+                                        </div>
+                                        <div className="col-3 text-right">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" className="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-md-6">
+                            <div className="card">
+                                <div className="card-block">
+                                    <div className="row align-items-center">
+                                        <div className="col-8">
+                                            <div className="card-inner-text">
+                                                <h4 className="text-c-yellow f-w-600">$30200</h4>
+                                                <h6 className="text-muted m-b-0">All Earnings</h6>
+                                            </div>
+                                        </div>
+                                        <div className="col-4 text-right">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-footer bg-c-red">
+                                    <div className="row align-items-center">
+                                        <div className="col-9">
+                                            <p className="text-white m-b-0">% change</p>
+                                        </div>
+                                        <div className="col-3 text-right">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" className="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-md-6">
+                            <div className="card">
+                                <div className="card-block">
+                                    <div className="row align-items-center">
+                                        <div className="col-8">
+                                            <div className="card-inner-text">
+                                                <h4 className="text-c-yellow f-w-600">$30200</h4>
+                                                <h6 className="text-muted m-b-0">All Earnings</h6>
+                                            </div>
+                                        </div>
+                                        <div className="col-4 text-right">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-footer bg-c-green">
+                                    <div className="row align-items-center">
+                                        <div className="col-9">
+                                            <p className="text-white m-b-0">% change</p>
+                                        </div>
+                                        <div className="col-3 text-right">
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" className="stretched-link"></a>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-md-6">
+                            <div className="card">
+                                <div className="card-block">
+                                    <div className="row align-items-center">
+                                        <div className="col-8">
+                                            <div className="card-inner-text">
+                                                <h4 className="text-c-yellow f-w-600">$30200</h4>
+                                                <h6 className="text-muted m-b-0">All Earnings</h6>
+                                            </div>
+                                        </div>
+                                        <div className="col-4 text-right">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-footer bg-c-blue">
+                                    <div className="row align-items-center">
+                                        <div className="col-9">
+                                            <p className="text-white m-b-0">% change</p>
+                                        </div>
+                                        <div className="col-3 text-right">
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" className="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Panel;
