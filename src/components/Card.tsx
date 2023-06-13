42cc213bca12b9508576528c98d88ef172deb8c3
NivekNK <kevin.rojas.v@mail.pucv.cl>
2023-06-12 23:34:51 -0400

    upate

---
+++ src/components/Card.tsx
@@ -0,0 +1,31 @@
+export function CardData() {
+    return (
+        <div className="col-xl-3 col-md-6">
+            <div className="card">
+                <div className="card-block">
+                    <div className="row align-items-center">
+                        <div className="col-8">
+                            <div className="card-inner-text">
+                                <h4 className="text-c-yellow f-w-600">$30200</h4>
+                                <h6 className="text-muted m-b-0">All Earnings</h6>
+                            </div>
+                        </div>
+                        <div className="col-4 text-right">
+                        </div>
+                    </div>
+                </div>
+                <div className="card-footer bg-c-yellow">
+                    <div className="row align-items-center">
+                        <div className="col-9">
+                            <p className="text-white m-b-0">% change</p>
+                        </div>
+                        <div className="col-3 text-right">
+
+                        </div>
+                    </div>
+                </div>
+                <a href="#" className="stretched-link"></a>
+            </div>
+        </div>
+    );
+}
